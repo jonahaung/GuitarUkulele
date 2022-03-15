35eaa78aab54abc15d63057a5f1fc8fcaae52f58
Aung Ko Min <jonahaung@gmail.com>
2022-03-15 21:39:58 +0800

    Initial Commit

---
+++ Shared/GuitarUkuleleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarUkuleleApp.swift
+//  Shared
+//
+//  Created by Aung Ko Min on 15/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GuitarUkuleleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
